464fa4f36d10cb9675336a77c7e3881bfc66b66d
you liang <tan_you_liang@hotmail.com>
2019-04-09 15:54:39 +0800

    backup icp testing

---
+++ icp_testing/slam_icp3d_simple_example/build/CMakeFiles/slam_icp3d_simple_example.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/youliang/Desktop/slam_icp3d_simple_example/test.cpp" "/home/youliang/Desktop/slam_icp3d_simple_example/build/CMakeFiles/slam_icp3d_simple_example.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/mrpt/slam/include"
+  "/usr/local/include/mrpt/vision/include"
+  "/usr/local/include/mrpt/obs/include"
+  "/usr/local/include/mrpt/opengl/include"
+  "/usr/local/include/mrpt/poses/include"
+  "/usr/local/include/mrpt/bayes/include"
+  "/usr/local/include/mrpt/math/include"
+  "/usr/local/include/mrpt/core/include"
+  "/usr/local/include"
+  "/usr/local/include/mrpt/containers/include"
+  "/usr/local/include/mrpt/typemeta/include"
+  "/usr/local/include/mrpt/serialization/include"
+  "/usr/local/include/mrpt/rtti/include"
+  "/usr/local/include/mrpt/random/include"
+  "/usr/local/include/mrpt/system/include"
+  "/usr/local/include/mrpt/nanoflann/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ icp_testing/slam_icp3d_simple_example/build/CMakeFiles/slam_icp3d_simple_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slam_icp3d_simple_example.dir/test.cpp.o"
+  "slam_icp3d_simple_example.pdb"
+  "slam_icp3d_simple_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slam_icp3d_simple_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
